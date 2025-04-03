08b0fb9f3550e3f3fc3796289938d2302de1b253
Yeswanth Reddy <yeswanthreddy546@gmail.com>
2025-04-03 19:51:33 -0400

    labassess2
    lab2

--- scripts/app.js
+++ scripts/app.js
@@ -3,13 +3,25 @@ const app = Vue.createApp({
     /// Define the data for the app
     data() {
         return {
+        fullName: 'Yeswanth Reddy',
+        pressuremonitorValue: 65,
 
+        pressureMonitorLevels: [
+            { id: 'danger-low', color: 'danger', label: 'Danger - Low!', min: 0, max:10 },
+            { id: 'warn-low', color: 'warning', label: 'Warning - Low', min: 11, max:30 },
+            { id: 'safe', color: 'safe', label: 'Safe', min: 31, max: 70 },
+            { id: 'warn-high', color: 'warning', label: 'Warning - High', min: 71,max: 90 },
+            { id: 'danger-high', color: 'danger', label: 'Danger - High', min: 91,max: 100 }
+        ]
         };
     },
+    
     /// Define the methods for the app
     methods: {
         isActive(pressureLevel) {
+            console.log(pressureLevel);
             /// Logic here for if the pressure is within the level parameters return boolean
+            return this.pressuremonitorValue>=pressureLevel.min&&this.pressuremonitorValue<=pressureLevel.max;
         }
     }
 });
